126671b9a422f1fae8519a2efe68d2bc464ca6af
Phusilachai_Thamwongsa <154107534+phusilachaiMoviLaid@users.noreply.github.com>
2023-12-18 11:07:26 +0700

    Add files via upload

---
+++ week1/index.php
@@ -0,0 +1,14 @@
+<?php
+    $address = "moo.2 tanlian";
+    $char1 = 'A';
+    echo $address[0].$address[1].$address[2].$address[3].$address[4];
+    echo "<br>";
+    echo $char1;
+    $age = 17;
+    $pi = 3.14;
+    $pi = 3.14144524;
+
+    echo $age+$pi;
+    $isTrue = false;
+    echo $isTrue;
+?>
